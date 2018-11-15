a98833a22cc5a21305421d75f3a9a3bf9dcb3f2a
shannongamby <shannongamby@hotmail.com>
2018-11-15 11:21:22 +0000

    method rename

--- src/Game.js
+++ src/Game.js
@@ -9,18 +9,18 @@ function Game() {
 }
 
 Game.prototype.addX = function(row, column) {
-  if (this.isAvailable(row, column)) {
+  if (this.spaceIsAvailable(row, column)) {
     this.scoreboard[row - 1][column - 1] = "X"
   }
 }
 
 Game.prototype.addO = function(row, column) {
-  if (this.isAvailable(row, column)) {
+  if (this.spaceIsAvailable(row, column)) {
     this.scoreboard[row - 1][column - 1] = "O"
   }
 }
 
-Game.prototype.isAvailable = function(row, column) {
+Game.prototype.spaceIsAvailable = function(row, column) {
   let choice = this.scoreboard[row - 1][column - 1]
   if (choice === "X" || choice === "O") {
     return false
